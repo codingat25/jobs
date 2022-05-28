441f6c729f3e7cf01f51bbe5fb5129297f279a95
ChristianReyes <christianquimporeyes@gmail.com>
2022-05-28 22:18:06 +0800

    fixed bad repo

---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corporis molestiae fuga, iure dolorem aliquid soluta aperiam. Tenetur esse ducimus laboriosam expedita magnam. Asperiores, amet fuga, accusamus nesciunt nam facere in excepturi numquam veniam consequuntur, aliquid possimus placeat maxime explicabo aliquam voluptate ut expedita quia temporibus modi magni? Aut eligendi dicta eius vitae, perferendis, harum optio ipsam eos id quod accusantium, quaerat quam. Quod neque ipsa suscipit saepe modi magnam ipsum placeat atque eveniet inventore, harum in quas blanditiis, voluptas necessitatibus ea fugiat animi iusto fugit accusantium itaque. Nostrum, aspernatur praesentium. Ea, eligendi sit rem veniam expedita quam quasi explicabo?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis excepturi deleniti aliquid rerum! Tempore laboriosam beatae quisquam odio deserunt explicabo assumenda incidunt ipsum cumque mollitia dolore omnis ea, iste sit.</p>
+  </div>
+</template>
